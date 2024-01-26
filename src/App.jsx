eb5135fa216e764f1126b912a442a1df11ddb8c5
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-26 23:38:52 +0100

    rendring all the component to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-
-import './App.css'
+import "./App.css";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Experience from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Navs from "./components/Navs/Navs";
+import PortFolio from "./components/PortFolio/PortFolio";
+import Service from "./components/Service/Service";
+import Testimonial from "./components/Testimonials/Testimonial";
 
 function App() {
-  
-
   return (
     <>
-     
+      <Header />
+      <Navs/>
+      <About/>
+      <Experience/>
+      <Service/>
+      <PortFolio/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
