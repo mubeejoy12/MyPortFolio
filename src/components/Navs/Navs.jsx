cc5237837e89d389fba6731d9d1519211e60d205
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-29 21:25:19 +0100

    useState was used on the links to make changes

--- src/components/Navs/Navs.jsx
+++ src/components/Navs/Navs.jsx
@@ -6,10 +6,40 @@ import { BiMessageAltDetail } from "react-icons/bi";
 const Navs = () => {
   const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>
-      Navs
-    </div>
-  )
-}
-
-export default Navs
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <RiBook2Line />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageAltDetail />
+      </a>
+    </nav>
