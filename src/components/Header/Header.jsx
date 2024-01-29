357eaf608a86860cc88acfe05c6e6ce272148a81
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-29 21:32:00 +0100

    header conponent have the introduction to the portfolio like the name, picture and his social media

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import me from '../../assets/me.png'
+import HeaderSocial from "./HeaderSocial";
+
+
 const Header = () => {
   return (
     <header>
@@ -7,6 +12,12 @@ const Header = () => {
         <h5>Hello I'm </h5>
         <h1>Abubakr Mubarak</h1>
        <h5 className="text-light">FullStack Developer</h5>
+       <CTA/>
+       <HeaderSocial/>
+       <div className="me">
+        <img src={me} alt="" className="img" />
+       </div>
+       <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   );
