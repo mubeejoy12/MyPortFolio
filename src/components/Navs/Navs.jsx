3a4a3dc7addb60dff2773de16244ca4e6f0003eb
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-29 21:22:23 +0100

    chore: imported react icons

--- src/components/Navs/Navs.jsx
+++ src/components/Navs/Navs.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import './navs.css'
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { RiBook2Line } from "react-icons/ri";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageAltDetail } from "react-icons/bi";
 const Navs = () => {
   return (
     <div>
