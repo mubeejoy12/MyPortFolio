0b2093aa3c2638415f363f3fe05604a30fd58a4c
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-29 21:33:13 +0100

    cta conponent contain the imported cv which can be doownload and his contacts

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import CV from "../../assets/cv.pdf"
+import React from "react";
+// import CV from "../../assets/MubarakCV.pdf"
+// import CV from "../../assets/MubarakCV.pdf";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-      <a href={CV} download>Download CV</a>
-      <a href="#contact">Let's Talk</a>
+    <div className="cta">
+      <a href="" className="btn" download>
+        Download CV
+      </a>
+      <a href="#contact" className="btn  btn-primary">Let's Talk</a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
