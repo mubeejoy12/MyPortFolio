9287765b723372011af378b5c221fb2c4a403a65
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-02-06 00:57:59 +0100

    functionality: added message js to the form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
 import './contact.css'
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_c1p542q", "template_pc3ty6d", form.current, {
+        publicKey: "O63jnZYpzforwG_gl",
+
+      }
+     
+      )
+      e.target.reset()
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <section id='contact'>
       Contact
