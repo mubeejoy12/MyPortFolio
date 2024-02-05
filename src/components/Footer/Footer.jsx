8248686a5aca8de3123615265af2b824930dd243
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-02-06 00:52:24 +0100

    adding content to the footer  component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,54 @@
+import React from "react";
+import "./footer.css";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Mubeejoy
+      </a>
+      <ul className="permalink">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonial">Testimonial</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FaSquareXTwitter />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Mubeejoy. All right reserved</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
