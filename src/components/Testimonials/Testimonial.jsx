b0127400f4fa320c8164444bfa0b5c6aeaf790aa
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-02-11 18:40:22 +0100

    adding some arrays of object

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -5,14 +5,13 @@ import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
 
-import { Pagination} from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
 
 const Testimonial = () => {
   const data = [
@@ -22,13 +21,50 @@ const Testimonial = () => {
       review:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitiaquasi harum omnis quae nemo, aliquam reiciendis dicta veniam totam,sed quaerat tenetur delectus accusamus iste dolores? Quasi laborum,eaque molestias assumenda ab totam praesentium iure eligendirecusandae, perferendis vitae voluptatum",
     },
+    {
+      avatar: AVTR2,
+      name: "Wright Steven",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitiaquasi harum omnis quae nemo, aliquam reiciendis dicta veniam totam,sed quaerat tenetur delectus accusamus iste dolores? Quasi laborum,eaque molestias assumenda ab totam praesentium iure eligendirecusandae, perferendis vitae voluptatum",
+    },
+    {
+      avatar: AVTR3,
+      name: "Mr Hammed Adekunle",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitiaquasi harum omnis quae nemo, aliquam reiciendis dicta veniam totam,sed quaerat tenetur delectus accusamus iste dolores? Quasi laborum,eaque molestias assumenda ab totam praesentium iure eligendirecusandae, perferendis vitae voluptatum",
+    },
+    {
+      avatar: AVTR4,
+      name: "Halimah Mufty",
+      review:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitiaquasi harum omnis quae nemo, aliquam reiciendis dicta veniam totam,sed quaerat tenetur delectus accusamus iste dolores? Quasi laborum,eaque molestias assumenda ab totam praesentium iure eligendirecusandae, perferendis vitae voluptatum",
+    },
   ];
   return (
     <section id="testimonial">
       <h5>Review from Clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials__container">
-        <article className="testimonial">
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide className="testimonial" key={index}>
+              <div className="client__avatar">
+                <img src={avatar} alt="" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+
+        {/* <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="" />
           </div>
@@ -40,34 +76,8 @@ const Testimonial = () => {
             eaque molestias assumenda ab totam praesentium iure eligendi
             recusandae, perferendis vitae voluptatum.
           </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            quasi harum omnis quae nemo, aliquam reiciendis dicta veniam totam,
-            sed quaerat tenetur delectus accusamus iste dolores? Quasi laborum,
-            eaque molestias assumenda ab totam praesentium iure eligendi
-            recusandae, perferendis vitae voluptatum.
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="" />
-          </div>
-          <h5 className="client__name">Ernest Achiever</h5>
-          <small className="client__review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            quasi harum omnis quae nemo, aliquam reiciendis dicta veniam totam,
-            sed quaerat tenetur delectus accusamus iste dolores? Quasi laborum,
-            eaque molestias assumenda ab totam praesentium iure eligendi
-            recusandae, perferendis vitae voluptatum.
-          </small>
-        </article>
-      </div>
+        </article> */}
+      </Swiper>
     </section>
   );
 };
