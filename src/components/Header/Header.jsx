c0c6031ed3f19ea0e476ee2fc15d963cfab9e8d0
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-02-16 10:10:04 +0100

    chore:rendering another component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import me from '../../assets/me.png'
-import meme from '../../assets/frame.png'
+import meme from "../../assets/frame.png";
 import HeaderSocial from "./HeaderSocial";
-
 
 const Header = () => {
   return (
@@ -12,13 +10,15 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm </h5>
         <h1>Abubakr Mubarak</h1>
-       <h5 className="text-light">FullStack Developer</h5>
-       <CTA/>
-       <HeaderSocial/>
-       <div className="me">
-        <img src={meme} alt="" className="img" />
-       </div>
-       <a href="#contact" className="scroll__down">Scroll Down</a>
+        <h5 className="text-light">FullStack Developer</h5>
+        <CTA />
+        <HeaderSocial />
+        <div className="me">
+          <img src={meme} alt="" className="img" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
